fd623782a6add41e58d3e60baddbaf2b831540e5
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-05-04 00:05:00 +0300

    Projectile
    Projectile added

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed = 1.0f;
+
+    void Update()
+    {
+        if(target == null) return;
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if(targetCapsule == null)
+        {
+            return target.position;
+        }
+        return target.position + Vector3.up * targetCapsule.height / 2;
+    }
+}
